d534633b5881d48972068343607a94ff34ff35c1
GaspardC <gaspard.chevassus-agnes@phelma.grenoble-inp.fr>
2016-06-13 18:59:29 +0200

    ask permissions for android 6.0

--- app/src/main/java/com/epfl/php/testphp/FirstActivity.java
+++ app/src/main/java/com/epfl/php/testphp/FirstActivity.java
@@ -1,6 +1,11 @@
 package com.epfl.php.testphp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +14,36 @@ import android.widget.Toast;
 
 public class  FirstActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CAMERA = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+
+        int permissionCameraCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int permissionWriteCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permissionInternetCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+        int permissionFineLocationCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+
+        if(permissionCameraCheck != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
+        }
+        if(permissionCameraCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+        if(permissionCameraCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 2);
+        }
+        if(permissionCameraCheck != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 3);
+        }
+
+
     }
+
 
     public void goToMainActivity(View view) {
         EditText user_id = (EditText) findViewById(R.id.user_id);
